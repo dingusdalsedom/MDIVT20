44c242f412fc25b70a8d80e7d848d43805e14610
Douglas Halse <4headmaster@gmail.com>
2020-03-31 13:39:10 +0200

    Basic debug text for hud. Garbage scaling so it was hard-coded so it does not sit properly when maximized

---
+++ Assets/Scenes/TestScene/Scripts/debugText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class debugText : MonoBehaviour
+{
+    private Canvas can;
+    private Text debug_text;
+    public string currently_looking_at;
+    public string duration_looked_at;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject canvObj = new GameObject("Canvas");
+        canvObj.transform.SetParent(this.transform);
+        can = canvObj.AddComponent<Canvas>();
+        RectTransform canvTransform = can.GetComponent<RectTransform>();
+        can.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        GameObject txtObj = new GameObject("Text");
+        debug_text = txtObj.AddComponent<Text>();
+        debug_text.transform.position = new Vector3(-319, 22, 0);
+        debug_text.rectTransform.sizeDelta = new Vector2(500, 570);
+        txtObj.transform.SetParent(can.transform, false);
+        debug_text.font = Font.CreateDynamicFontFromOSFont("Arial", 4);
+        debug_text.alignment = TextAnchor.UpperLeft;
+        debug_text.color = Color.black;
+        debug_text.text = "Sight tracker";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        debug_text.text = "Sight tracker debug information: \n" +
+            "Currently looking at: " + currently_looking_at + "\n" +
+            "Duration looked at: " + duration_looked_at + "\n";
+    }
+}
