f027b848f47208b730e3fe32ce1a08e136c28e55
Mattias <mk-98@live.se>
2020-04-01 16:05:12 +0200

    Raycast
    Added raycast script. Added script for storing total time looked at  + timestamps for objects

---
+++ Assets/Scenes/TestScene/Scripts/TimeInfoObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeInfoObject : MonoBehaviour
+{
+    public float timeLookedAt;      //total time looked at this object (seconds)
+    public List<string> timeStamps;     //all timestamps (min:sec) when object is looked at
+    public string timeStamp;        //current timestamp when the object was look at
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLookedAt = 0;
+        timeStamps = new List<string>();
+        timeStamp = "0:00";
+    }
+
+}
