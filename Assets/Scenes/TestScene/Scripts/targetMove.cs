4fa0c1b230b2f55d464e7e3702040a9176a9a628
Marcus <marosh171@studentmail.oru.se>
2020-04-07 10:48:58 +0200

    Added different directions, code cleanup

--- Assets/Scenes/TestScene/Scripts/targetMove.cs
+++ Assets/Scenes/TestScene/Scripts/targetMove.cs
@@ -7,6 +7,7 @@ public class targetMove : MonoBehaviour
     Vector3 pos = new Vector3();
     bool direction = true;
     public float speed = 0.05f;
+    public char movementDirection = 'x';
 
     void Start()
     {
@@ -15,28 +16,73 @@ public class targetMove : MonoBehaviour
 
     void Update()
     {
-        // 44 to 180
-        //Vector3 pos = gameObject.transform.position;
-        
-        if(direction)
+        // 180 and 44 is the coordinates in X for the "room" of the shooting range.
+        // 130 and 82 is the coordinates in X for the "room" of the shooting range.
+        // 330 and 161 is the coordinates in Z for the "room" of the shooting range.
+
+        if (movementDirection == 'x')
         {
-            pos.x = pos.x + speed;
-        }
-        else
-        {
-            pos.x = pos.x - speed;
+            if(direction)
+            {
+                pos.x = pos.x + speed;
+            }
+            else
+            {
+                pos.x = pos.x - speed;
+            }
+            if (pos.x > 180f)
+            {
+                direction = false;
+            }
+            if (pos.x < 44f)
+            {
+                direction = true;
+            }
         }
 
+        else if (movementDirection == 'y')
+        {
+            if (direction)
+            {
+                pos.y = pos.y + speed;
+            }
+            else
+            {
+                pos.y = pos.y - speed;
+            }
+            if (pos.y > 130f)
+            {
+                direction = false;
+            }
+            if (pos.y < 82f)
+            {
+                direction = true;
+            }
+        }
+
+        else if (movementDirection == 'z')
+        {
+            if (direction)
+            {
+                pos.z = pos.z + speed;
+            }
+            else
+            {
+                pos.z = pos.z - speed;
+            }
+            if (pos.z > 330f)
+            {
+                direction = false;
+            }
+            if (pos.z < 161f)
+            {
+                direction = true;
+            }
+        }
+               
         gameObject.transform.localPosition = pos; 
         
-        if(pos.x > 180f)
-        {
-            direction = false;
-        }
-        if(pos.x < 44)
-        {
-            direction = true;
-        }
-
+        
+        
     }
 }
