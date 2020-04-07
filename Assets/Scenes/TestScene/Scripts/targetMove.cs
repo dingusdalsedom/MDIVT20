87e09182ddb66f8320bc4ed5a385015561f5126c
Marcus <marosh171@studentmail.oru.se>
2020-04-07 10:33:46 +0200

    Added script for moving targets

---
+++ Assets/Scenes/TestScene/Scripts/targetMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetMove : MonoBehaviour
+{
+    Vector3 pos = new Vector3();
+    bool direction = true;
+    public float speed = 0.05f;
+
+    void Start()
+    {
+        pos = gameObject.transform.localPosition;
+    }
+
+    void Update()
+    {
+        // 44 to 180
+        //Vector3 pos = gameObject.transform.position;
+        
+        if(direction)
+        {
+            pos.x = pos.x + speed;
+        }
+        else
+        {
+            pos.x = pos.x - speed;
+        }
+
+        gameObject.transform.localPosition = pos; 
+        
+        if(pos.x > 180f)
+        {
+            direction = false;
+        }
+        if(pos.x < 44)
+        {
+            direction = true;
+        }
+
+    }
+}
